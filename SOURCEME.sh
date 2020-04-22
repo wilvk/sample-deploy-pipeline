85b8c518042220dda3d12335eaedc8abee28ff17
wilvk <willvk@gmail.com>
2020-04-22 21:43:22 +1000

    update to use dockerhub

--- SOURCEME.sh
+++ SOURCEME.sh
@@ -5,24 +5,25 @@ export APPSEC_TEXT_START="\033[1;32m"
 export APPSEC_TEXT_END="\033[0m"
 export APPSEC_BOLD_START="\033[0m\033[1m"
 export APPSEC_BOLD_END="\033[1;32m"
+export APPSEC_IMAGE_VERSION="will01/appsec-101:0.1.0"
 
 function appsec_colour_echo() {
     echo "$APPSEC_TEXT_START $1 $APPSEC_TEXT_END"
 }
 
-function appsec_build() {
-    docker build -t jenkins-docker .
+function appsec_pull() {
+    docker pull ${APPSEC_IMAGE_VERSION}
 }
 
 function appsec_start_jenkins() {
-    docker run -it -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped jenkins-docker 1>/dev/null
+    docker run -it -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped ${APPSEC_IMAGE_VERSION} 1>/dev/null
 }
 
 function appsec_get_jenkins_admin_creds() {
-    DOCKER_IMAGE=$(docker ps -qf "ancestor=jenkins-docker")
+    DOCKER_IMAGE=$(docker ps -qf "ancestor=${APPSEC_IMAGE_VERSION}")
     if [ -z "$DOCKER_IMAGE" ];
     then
-      appsec_colour_echo "    -> error: no docker container found with name jenkins-docker"
+      appsec_colour_echo "    -> error: no docker container found with name ${APPSEC_IMAGE_VERSION}"
     else
       ADMIN_USER=$(docker exec -it $DOCKER_IMAGE cat /var/jenkins_home/secrets/initialAdminPassword)
       appsec_colour_echo "    -> admin password: ${APPSEC_BOLD_START}${ADMIN_USER}${APPSEC_BOLD_END}"
@@ -35,25 +36,25 @@ function appsec_list() {
 }
 
 function appsec_get_jenkins_shell() {
-    docker exec -it $(docker ps -qf "ancestor=jenkins-docker") bash
+    docker exec -it $(docker ps -qf "ancestor=${APPSEC_IMAGE_VERSION}") bash
 }
 
 function appsec_stop() {
-    DOCKER_IMAGE=$(docker ps -qf "ancestor=jenkins-docker") 1>/dev/null
+    DOCKER_IMAGE=$(docker ps -qf "ancestor=${APPSEC_IMAGE_VERSION}") 1>/dev/null
     if [ -z "$DOCKER_IMAGE" ];
     then
-      appsec_colour_echo "  -> no docker container found with name jenkins-docker"
+      appsec_colour_echo "  -> no docker container found with name ${APPSEC_IMAGE_VERSION}"
     else
       docker stop $DOCKER_IMAGE
-      appsec_colour_echo "  -> docker container with name jenkins-docker and id $DOCKER_IMAGE has been stopped."
+      appsec_colour_echo "  -> docker container with name ${APPSEC_IMAGE_VERSION} and id $DOCKER_IMAGE has been stopped."
     fi
 }
 
 function appsec_cleanup() {
-    appsec_colour_echo "  -> stopping jenkins-docker container..."
+    appsec_colour_echo "  -> stopping ${APPSEC_IMAGE_VERSION} container..."
     appsec_stop
-    appsec_colour_echo "  -> removing jenkins-docker image..."
-    docker image rm jenkins-docker --force
+    appsec_colour_echo "  -> removing ${APPSEC_IMAGE_VERSION} image..."
+    docker image rm ${APPSEC_IMAGE_VERSION} --force
     appsec_colour_echo "  -> pruning dangling images..."
     docker image prune --force
     appsec_colour_echo "  -> pruning dangling containers..."
@@ -63,12 +64,12 @@ function appsec_cleanup() {
 }
 
 function appsec_start() {
-    appsec_colour_echo "Starting appsec sample-deploy-pipeline..."
+    appsec_colour_echo "Starting appsec sample-deploy-pipeline with docker image ${APPSEC_IMAGE_VERSION}..."
     appsec_colour_echo "  -> Cleaning up any previous run of sample-deploy-pipeline..."
     appsec_cleanup
-    appsec_colour_echo "  -> Building docker image for sample-deploy-pipeline..."
-    appsec_build
-    appsec_colour_echo "  -> Starting docker container for sample-deploy-pipeline..."
+    appsec_colour_echo "  -> Pulling docker image for ${APPSEC_IMAGE_VERSION}..."
+    appsec_pull
+    appsec_colour_echo "  -> Starting docker container for ${APPSEC_IMAGE_VERSION}..."
     appsec_start_jenkins
     appsec_colour_echo "  -> Sleeping for 30 seconds before retrieving credentials..."
     sleep 30s
